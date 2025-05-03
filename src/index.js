820dea768bcbafbdf9929e9d8a174caef8643b40
A-Eren512 <eren_2003_akcay@hotmail.com>
2025-05-03 22:46:59 +0300

    feat: add closeHour

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ function App() {
 
   const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 22;
+  const closeHour = 21;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   const toggleDarkMode = () => {
